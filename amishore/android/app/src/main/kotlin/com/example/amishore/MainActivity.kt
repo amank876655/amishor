7f639068c6ae2f7fa74cd4e810a001e3ad322c68
amank876655 <amank876655@gmail.com>
2022-05-25 23:04:12 +0530

    first commit

---
+++ amishore/android/app/src/main/kotlin/com/example/amishore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amishore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ amishot/android/app/src/main/kotlin/com/example/amishot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amishot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
